3af846ba34a49d954372fdd689f4287712ab9576
Ginestra Ferraro <ginestra.ferraro@kcl.ac.uk>
2016-06-15 15:32:49 +0100

    Search box

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,28 +9,5 @@ require([
     'use strict';
 
     $(document).ready(function() {
-
-	    // $('toggle').click(function () {
-	    //     var trigger = $(this),
-	    //         inputForm = trigger.siblings('#searchform'),
-	    //         expanded = inputForm.is("expanded")
-
-	    //         if (expanded) {
-	    //             inputForm.removeClass('expanded').animate({
-	    //                 right: 100
-	    //             })
-	    //         } else {
-	    //             inputForm.animate({
-	    //                 right: 48
-	    //             }, function () {
-	    //                 inputForm.addClass('expanded').find('input').focus()
-	    //             })
-	    //         }
-	    // })
-
-	    $(".form input").blur(function () {
-	        $(this).closest('.form').find('.toggle').click()
-	    })
-
     });
 });
