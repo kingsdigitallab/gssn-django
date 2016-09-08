dc9670e67392a7dffefb8153c97a01ab506f4d11
Ginestra Ferraro <ginestra.ferraro@kcl.ac.uk>
2016-09-08 22:44:33 +0100

    Added field and styling for copyright in carousel

--- assets/js/config.require.js
+++ assets/js/config.require.js
@@ -30,7 +30,7 @@ requirejs.config({
         'foundation.util.touch': '../vendor/foundation-sites/js/foundation.util.touch',
         'foundation.util.triggers': '../vendor/foundation-sites/js/foundation.util.triggers',
 
-        'js-cookie': '../vendor/js-cookie/src/js.cookie'
+        'js-cookie': '../vendor/js-cookie/src/js.cookie',
 
         'slick': '../vendor/slick-carousel/slick/slick',
 
